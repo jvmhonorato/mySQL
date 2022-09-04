00434e29773a1a6e5c26b8d73020622b87a05f1f
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-09-03 23:15:10 -0300

    finish migration

--- db.js
+++ db.js
@@ -2,21 +2,21 @@ const mysql = require('mysql2/promise')
 
 
 //WHITOUT CONNECTION POOL
-//     module.exports =  mysql.createConnection({
-//         host:'localhost',
-//         user: 'root',
-//         database: 'cat-products'
-// })
+    module.exports =  mysql.createConnection({
+        host:'localhost',
+        user: 'root',
+        database: 'cat-products'
+})
 
 //WITH CONNECTION POOL
-module.exports =  mysql.createPool({
-    host:'localhost',
-    user: 'root',
-    database: 'cat-products',
-    waitForConnections: true,
-    connectionLimit: 20,
-    queueLimit: 0
-})
+// module.exports =  mysql.createPool({
+//     host:'localhost',
+//     user: 'root',
+//     database: 'cat-products',
+//     waitForConnections: true,
+//     connectionLimit: 20,
+//     queueLimit: 0
+// })
 
 
 
--- migration.js
+++ migration.js
@@ -1,5 +1,6 @@
 const db =require('./db')
 const fs = require('fs')
+const { Console } = require('console')
 
 const initMigration = async(connection) => {
     const [results] = await connection.query(`show tables like 'migration_version'`)
@@ -16,6 +17,12 @@ const initMigration = async(connection) => {
         await connection.query('COMMIT;')
     }
 }
+const getCurrentVersion = async(connection) => {
+    const [ results ] = await connection.query('select * from migration_version where id = 1')
+     return results[0].version
+
+    
+}
 
 
 const migration = async()=> {
@@ -23,10 +30,19 @@ const migration = async()=> {
     await initMigration(connection)
 
     const currentVersion = await  getCurrentVersion(connection)
-    const targetVersion = 1000
+    //DEIXAR MIGRAÇÃO DINÂMICA COM PARÂMETROS
+    let targetVersion =   1000
+    if(process.argv.length > 2){
+    if(process.argv[2] === '--target-version'&& process.argv[3]){
+        targetVersion = parseInt(process.argv[3])
+    }
 
-  
+  }
+  console.log('Migrating to: ', targetVersion)
     const migrations = fs.readdirSync('./migrations')
+
+
+
 
     //FUNÇÃO PRA ORDENAR RETORNO DA LISTA DE MIGRATION
     const migrationSorted = migrations.map(version => {
@@ -36,10 +52,17 @@ const migration = async()=> {
     .sort((a,b) => {
         if(a>b){
             return 1
-        }return -1
+        }
+        return -1
     })
-    migrationSorted
-    console.log()
+    
+    
+    const migrationSorted2 = [...migrationSorted].sort((a,b)=> {
+        if(a>b){
+            return -1
+        }return 1
+    })
+    
 
         //USANDO O FOR EACH
       // const migrations = fs.readdirSync('./migrations')
@@ -47,7 +70,8 @@ const migration = async()=> {
       //     const m = require('./migrations/'+migration)
       //     m.up(connection)
       // })
-
+      
+      //UP
         //USANDO O FOR AWAIT
         for await (const migration of migrationSorted ) {
             if(migration > currentVersion && targetVersion >= migration){
@@ -56,19 +80,40 @@ const migration = async()=> {
         await connection.query('START TRANSACTION;')
         if(m.up){
            await m.up(connection)
+           console.log('Migration UP', migration)
         }
         await connection.query('update migration_version set version = ? where id= ?',[migration, 1])
         await connection.query('COMMIT;')
         }
-        //USAR 'UP' PRA CRIAR TABLES E 'DOWN' PRA REMOVER TABELAS
+       
          
     }
+
+   //DOWN
+      for await(const migration of migrationSorted2){
+        if(migration <= currentVersion && targetVersion <= migration){
+            
+            const m = require('./migrations/'+migration+'.js')
+
+        await connection.query('START TRANSACTION;')
+        if(m.down){
+           await m.down(connection)
+           console.log('Migration DOWN', migration)
+        } 
+        const currentMigration = migrationSorted2[migrationSorted2.indexOf(migration)+1] || 0
+        await connection.query('update migration_version set version = ? where id= ?',[currentMigration, 1])
+       
+        await connection.query('COMMIT;')
+        }
+      }
+
+
+await connection.close()
    
 }
+    
 
-const getCurrentVersion = async(connection) => {
-    const [ results ] = await connection.query('select * from migration_version where id = 1')
-     return results[0].version
-}
+
+
 
 migration()
